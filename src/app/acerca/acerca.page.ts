6d2867a5bb4c3a88c08c05be4a619a942a77aae6
Cristobal Maturana <cri.maturana@gmail.com>
2026-08-30 16:31:45 -0400

    Solución del ejercicio: hola-app 1
    Paso 1: página con input, acción y saludo condicional.
    Paso 2: segunda página, rutas con loadComponent y navegación con routerLink e ion-back-button.

---
+++ src/app/acerca/acerca.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonBackButton,
+} from '@ionic/angular';
+
+@Component({
+  selector: 'app-acerca',
+  templateUrl: './acerca.page.html',
+  styleUrls: ['./acerca.page.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButtons,
+    IonBackButton,
+  ],
+})
+export class AcercaPage {}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonInput,
+  IonButton,
+} from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonInput,
+    IonButton,
+    FormsModule, // habilita [(ngModel)]
+    RouterLink, // habilita routerLink
+  ],
+})
+export class HomePage {
+  nombre = ''; // se conecta con el input
+  saludo = ''; // lo que mostramos al apretar el botón
+
+  saludar() {
+    this.saludo = this.nombre ? `¡Hola, ${this.nombre}!` : '¿Cómo te llamas?';
+  }
+}
